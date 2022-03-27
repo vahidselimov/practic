2fafddb754c76a259c14562970e55383dd054449
vahidselimov <100331397+vahidselimov@users.noreply.github.com>
2022-03-27 10:19:25 +0400

    task1

---
+++ Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            arr();
+        }
+        public static void arr()
+        {
+            int[] arr = { 9, 12, 34, 21, 56, 78, 43, 23, 90, 65,90 };
+            int max = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+
+                }
+            }
+            Console.WriteLine(max);
+        }
+
+
+
+    }
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
